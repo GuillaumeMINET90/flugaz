d86ae8caa0b555bb5ae4d28ff8e60aff760d0e81
DJIL <guillaumeminet90@gmail.com>
2022-10-25 23:39:07 +0200

    Suppression du TargetPath dans le LoginController

--- src/Security/Authenticator.php
+++ src/Security/Authenticator.php
@@ -42,9 +42,9 @@ class Authenticator extends AbstractLoginFormAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
-            return new RedirectResponse($targetPath);
-        }
+        // if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+        //     return new RedirectResponse($targetPath);
+        // }
 
          return new RedirectResponse($this->urlGenerator->generate('app_menu'));
     }
