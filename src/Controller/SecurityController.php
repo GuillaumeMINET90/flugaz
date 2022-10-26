da5b2589f9d02a533b73149af4728d4a5ed78fe0
DJIL <91847169+GuillaumeMINET90@users.noreply.github.com>
2022-10-26 23:46:55 +0200

    debug /login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,7 +9,14 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/', name: 'app_login')]
+    #[Route(path: '/', name: 'app_redirection')]
+    public function redirection(): Response
+    {
+
+        return $this->redirectToRoute('app_login', [], Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // if ($this->getUser()) {
